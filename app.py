d373c318244aa327c8a3587422403a111c0074c4
Lucas <lucasliao0403@gmail.com>
2025-01-12 09:12:56 -0500

    changed potr

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def get_map_data():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
